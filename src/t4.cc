b5c612c6f2d7684aadf40ffdfd7e884845c18387
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2009-07-06 14:30:57 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@394 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/t4.cc
+++ src/t4.cc
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <poll.h>
+#include <sys/select.h>
 
 #include "tools.h"
 #include "ext_socket.h"
@@ -9,17 +11,38 @@
 int main(int argc, char** argv)
 {
    if(argc <= 1) {
-	int sock = createAndBindSocket(SOCK_SEQPACKET, IPPROTO_SCTP, 9001);
+	int sock = createAndBindSocket(SOCK_SEQPACKET, IPPROTO_SCTP, 9001);	
 	if(sock) {
+	printf("Waiting on socket %d\n", sock);
+		sctp_event_subscribe events;
+		memset((char*)&events, 0 ,sizeof(events));
+		events.sctp_data_io_event     = 1;
+		events.sctp_association_event = 1;
+		if(ext_setsockopt(sock, IPPROTO_SCTP, SCTP_EVENTS, &events, sizeof(events)) < 0) {
+		exit(1);
+		}
 		char buffer[60000];
 		for(;;) {
-			ssize_t r = ext_recv(sock, (char*)&buffer, sizeof(buffer), 0);
-			printf("r=%d\n", r);
+		/*
+		        pollfd pfd;
+		        pfd.fd = sock;
+		        pfd.events = POLLIN;
+		        int result = ext_poll((pollfd*)&pfd, 1, 1000);
+		*/        
+		        fd_set rset;
+		        FD_ZERO(&rset);
+		        FD_SET(sock, &rset);
+		        int result = ext_select(sock + 1, &rset, NULL, NULL, NULL);
+		        
+		        if(result > 0) {
+   			   ssize_t r = ext_recv(sock, (char*)&buffer, sizeof(buffer), 0);
+ 			   printf("r=%d\n", r);
+                        }
 		}
 	}
    }
    else {
-      int sd = ext_socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+      int sd = ext_socket(AF_INET6, SOCK_STREAM, IPPROTO_SCTP);
 printf("sd=%d\n",sd);
       sockaddr_union in;
       if(string2address(argv[1],&in)) {
