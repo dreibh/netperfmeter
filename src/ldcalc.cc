b491cd06cbe6ea58ad302cd325f430340ae56c76
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2009-10-16 20:46:20 +0000

    Added TD EdgeDevice QDisc sources.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@507 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

---
+++ src/ldcalc.cc
@@ -0,0 +1,69 @@
+// $Id: createsummary.cc 328 2009-06-24 09:22:01Z dreibh $
+// ###########################################################################
+//             Thomas Dreibholz's R Simulation Scripts Collection
+//                  Copyright (C) 2004-2009 Thomas Dreibholz
+//
+//           Author: Thomas Dreibholz, dreibh@iem.uni-due.de
+// ###########################################################################
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: dreibh@iem.uni-due.de
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+   if(argc < 4) {
+      std::cerr << "Usage: " << argv[0] << " [Number 1] [Operator] [Number 2] {-exponential}" << std::endl;
+      exit(1);
+   }
+   
+   const long double number1 = strtold(argv[1], NULL);
+   const long double number2 = strtold(argv[3], NULL);
+   long double       result  = 0.0;
+   
+   if(strcmp(argv[2], "+") == 0) {
+      result = number1 + number2;
+   }
+   else if(strcmp(argv[2], "-") == 0) {
+      result = number1 - number2;
+   }
+   else if(strcmp(argv[2], "*") == 0) {
+      result = number1 * number2;
+   }
+   else if(strcmp(argv[2], "/") == 0) {
+      result = number1 / number2;
+   }
+   else {
+      std::cerr << "ERROR: Invalid operator " << argv[2] << "!" << std::endl;
+      exit(1);
+   }
+   
+   char str[128];
+   if( (argc > 4) && (strcmp(argv[4], "-exponential") == 0) ) {
+      snprintf((char*)&str, sizeof(str), "%1.15Le", result);
+   }
+   else {
+      snprintf((char*)&str, sizeof(str), "%1.15Lf", result);
+   }
+   std::cout << str << std::endl;
+   
+   return 0;
+}
