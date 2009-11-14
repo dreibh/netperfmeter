e2f027a5fce2341b11dc0dcb04c4d3dd97a8731b
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2009-11-14 23:00:20 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@518 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/plot-OMNeT++Workshop2010.R
+++ src/plot-OMNeT++Workshop2010.R
@@ -33,11 +33,11 @@ plotConfigurations <- list(
    # ------------------------------------------------------------------------
 
    list("wp0-links-symmetrischI", "paper2-wp0-links-symmetrischI-Throughput.pdf",
-        "FreeBSD Measurement Results", seq(0,100000,10000), NA, list(0,1),
+        "FreeBSD 8.0RC Measurement Results", seq(0,100000,10000), NA, list(0,1),
         "RateNorthernTrail-CM", "passive.total-ReceivedBitRate",
         "OptionCMT", "OptionDAC", "OptionNRSACK",
         "", "", "Unordered",
-        "TRUE")
+        "(data1$OptionDAC == \"true\")")
 )
 
 
--- src/plotter.R
+++ src/plotter.R
@@ -461,12 +461,22 @@ plotstd3 <- function(mainTitle,
                else if((type == "lx") || (type=="linesx")) {
                   xSubset <- subset(xSet, (zSet == z) & (vSet == v) & (wSet == w))
                   ySubset <- subset(ySet, (zSet == z) & (vSet == v) & (wSet == w))
-                  lines(xSubset, ySubset, col=legendColor, cex=par("cex"), pch=legendDot)
+                  lineWidth <- 5
+                  if((length(vLevels) > 1) || (length(wLevels) > 1)) {
+                     lineWidth <- 3
+                  }
+                  lines(xSubset, ySubset, col=legendColor, lty=legendStyle, cex=lineWidth*par("cex"), pch=legendDot)
 
                   legendTexts <- append(legendTexts, legendText)
                   legendColors <- append(legendColors, legendColor)
                   legendStyles <- append(legendStyles, legendStyle)
                   legendDots <- append(legendDots, legendDot)
+
+                  points(xSubset, ySubset,
+                        col=legendColor, lty=legendStyle, pch=legendDot,
+                        lwd=par("cex"),
+                        cex=par("cex"), bg="yellow")
+                  legendDot <- legendDot + 1
                }
 
                # ----- Horizontal bars plot ---------------------------------
@@ -484,6 +494,8 @@ plotstd3 <- function(mainTitle,
                   legendColors <- append(legendColors, legendColor)
                   legendStyles <- append(legendStyles, legendStyle)
                   legendDots   <- append(legendDots, legendDot)
+
+                  legendDot <- legendDot + 1
                }
 
                # ----- Lines or Steps plot ----------------------------------
@@ -563,14 +575,10 @@ plotstd3 <- function(mainTitle,
                      legendStyles <- append(legendStyles, legendStyle)
                      legendDots <- append(legendDots, legendDot)
 
-                     pcex <- par("cex")
-                     # if(length(wLevels) > 1) {
-                        pcex <- 2 * pcex
-                     # }
                      points(xPlotSet, yPlotMeanSet,
                            col=legendColor, lty=legendStyle, pch=legendDot,
                            lwd=par("cex"),
-                           cex=pcex,bg="yellow")
+                           cex=2*par("cex"), bg="yellow")
                      legendDot <- legendDot + 1
                   }
                }
