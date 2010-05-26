209aa54e8c6ceab456318e4262210b869f2154f4
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2010-05-26 16:34:13 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@551 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

---
+++ src/plot-test2.R
@@ -0,0 +1,49 @@
+# ###########################################################################
+# Name:        test1
+# Description:
+# Revision:    $Id: plot-wp0-links-symmetrischI.R 534 2010-01-22 12:29:37Z dreibh $
+# ###########################################################################
+
+source("plot-version1.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+simulationDirectory  <- "TEST2"
+plotColorMode        <- cmColor
+plotHideLegend       <- FALSE
+plotLegendSizeFactor <- 0.8
+plotOwnOutput        <- FALSE
+plotFontFamily       <- "Helvetica"
+plotFontPointsize    <- 22
+plotWidth            <- 10
+plotHeight           <- 10
+plotConfidence       <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   # ------ Format example --------------------------------------------------
+   # list(simulationDirectory, "output.pdf",
+   #      "Plot Title",
+   #      list(xAxisTicks) or NA, list(yAxisTicks) or NA, list(legendPos) or NA,
+   #      "x-Axis Variable", "y-Axis Variable",
+   #      "z-Axis Variable", "v-Axis Variable", "w-Axis Variable",
+   #      "a-Axis Variable", "b-Axis Variable", "p-Axis Variable")
+   # ------------------------------------------------------------------------
+
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-ReceivedBitRate.pdf"),
+        "Receiver's Perspective", NA, seq(0,200,25), list(0,1),
+        "RateSouthernTrail", "passive.flow-ReceivedBitRate-Mbit",
+        "OptionNRSACK", "BidirectionalQoS", "OptionRP",
+        "passive.flow", "OptionCMT", "Unordered")
+)
+
+
+# ------ Variable templates -------------------------------------------------
+plotVariables <- append(list(
+), netPerfMeterPlotVariables)
+
+# ###########################################################################
+
+createPlots(simulationDirectory, plotConfigurations)
