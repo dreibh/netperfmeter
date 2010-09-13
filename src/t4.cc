90153330612ae10f0e69bb184659ac31a155193e
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2010-09-13 20:20:49 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@747 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/t4.cc
+++ src/t4.cc
@@ -1,62 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <math.h>
-#include <poll.h>
+#include <sys/socket.h>
 #include <sys/select.h>
-
-#include "tools.h"
-#include "ext_socket.h"
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
 
 
 int main(int argc, char** argv)
 {
    if(argc <= 1) {
-      int sd = createAndBindSocket(SOCK_SEQPACKET, IPPROTO_SCTP, 9001);
-      if(sd >= 0) {
-         printf("Warte auf Socket %d ...\n", sd);
+      puts("SERVER MODE");
 
-         sctp_event_subscribe events;
-         memset((char*)&events, 0 ,sizeof(events));
-         events.sctp_data_io_event     = 1;
-         events.sctp_association_event = 1;
-         if(ext_setsockopt(sd, IPPROTO_SCTP, SCTP_EVENTS, &events, sizeof(events)) < 0) {
-            exit(1);
-         }
+      sockaddr_in6 local;
+      memset(&local, 0, sizeof(local));
+      local.sin6_family = AF_INET6;
+      local.sin6_port   = htons(8000);
 
-         char buffer[65536];
-         for(;;) {
-         /*
-            pollfd pfd;
-            pfd.fd = sd;
-            pfd.events = POLLIN;
-            int result = ext_poll((pollfd*)&pfd, 1, 1000);
-         */        
-            fd_set rset;
-            FD_ZERO(&rset);
-            FD_SET(sd, &rset);
-            int result = ext_select(sd + 1, &rset, NULL, NULL, NULL);
-                 
-            if(result > 0) {
-               ssize_t r = ext_recv(sd, (char*)&buffer, sizeof(buffer), 0);
-               printf("r=%d\n", (int)r);
+      int sd = socket(AF_INET6, SOCK_STREAM, IPPROTO_SCTP);
+      if(sd < 0) {
+         perror("socket()");
+         exit(1);
+      }
+
+      if(bind(sd, (struct sockaddr*)&local, sizeof(local)) < 0) {
+         perror("bind()");
+         exit(1);
+      }
+
+      if(listen(sd, 5) < 0) {
+         perror("bind()");
+         exit(1);
+      }
+
+      for(;;) {
+         int sd2 = accept(sd, NULL, NULL);
+         if(sd2 >= 0) {
+            char data[1404];
+            memset(&data, 'A', sizeof(data));
+            for(;;) {
+               if(send(sd2, &data, sizeof(data), MSG_NOSIGNAL) < 0) {
+                  perror("send()");
+                  break;
+               }
+//                printf("."); fflush(stdout);
+//                usleep(100000);
             }
+            close(sd2);
          }
       }
    }
+
    else {
-      int sd = ext_socket(AF_INET6, SOCK_STREAM, IPPROTO_SCTP);
-      if(sd >= 0) {
-         sockaddr_union in;
-         if(string2address(argv[1], &in)) {
-            puts("Connect ...");
-            if(ext_connect(sd, &in.sa, getSocklen(&in.sa)) == 0) {
-               char buffer[65536];
-               printf("sent=%d\n", (int)ext_send(sd, (char*)&buffer, 1024,0));
+      puts("CLIENT MODE");
+
+      sockaddr_in6 remote;
+      memset(&remote, 0, sizeof(remote));
+      remote.sin6_family = AF_INET6;
+      remote.sin6_port   = htons(8000);
+      if(inet_pton(AF_INET6, argv[1], &remote.sin6_addr) != 1) {
+         perror("inet_pton()");
+         exit(1);
+      }
+
+      int sd = socket(AF_INET6, SOCK_STREAM, IPPROTO_SCTP);
+      if(sd < 0) {
+         perror("socket()");
+         exit(1);
+      }
+
+      if(connect(sd, (struct sockaddr*)&remote, sizeof(remote)) < 0) {
+         perror("connect()");
+         exit(1);
+      }
+
+         struct sctp_paddrparams paddrparams;
+         memset(&paddrparams, 0, sizeof(paddrparams));
+         paddrparams.spp_hbinterval = 250;
+         paddrparams.spp_flags      = SPP_HB_ENABLE|SPP_HB_DEMAND;
+         memcpy(&paddrparams.spp_address, &remote, sizeof(remote));
+         if(setsockopt(sd, IPPROTO_SCTP, SCTP_PEER_ADDR_PARAMS, &paddrparams, sizeof(paddrparams)) < 0) {
+            perror("setsockopt()");
+            exit(1);
+         }
+
+      int n = 0;
+      while(n < 1000000) {
+
+         fd_set rset;
+         FD_ZERO(&rset);
+         FD_SET(sd, &rset);
+         int result = select(sd + 1, &rset, NULL, NULL, NULL);
+
+         if(result > 0) {
+            char buffer[65536];
+            ssize_t r = recv(sd, (char*)&buffer, sizeof(buffer), MSG_NOSIGNAL);
+            if(r < 0) {
+               perror("recv()");
+               exit(1);
             }
-            ext_close(sd);
+            n++;
+            if((n % 1000) == 0) {
+               printf("%d: received %d bytes\n", n, (int)r);
+            }
          }
       }
+      close(sd);
    }
    return 0;
 }
