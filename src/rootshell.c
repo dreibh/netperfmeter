2dc7c1fe0a66f80ac2818b64a483be0e2ed8514f
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2010-11-20 15:00:53 +0000

    Added option to set current directory.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@800 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/rootshell.c
+++ src/rootshell.c
@@ -30,17 +30,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 
 int main(int argc, char** argv)
 {
+   const char* directory  = NULL;
+   int         programArg = 1;
+   int         i;
+
    if(argc < 2) {
-      fprintf(stderr, "Usage: %s [Program] {Argument} ...\n", argv[0]);
+      fprintf(stderr, "Usage: %s {-chdir=Directory} [Program] {Argument} ...\n", argv[0]);
       exit(1);
    }
 
+   for(i = 1; i < argc; i++) {
+      if(argv[i][0] == '-') {
+         programArg = i + 1;
+         if(strncmp(argv[i], "-chdir=", 7) == 0) {
+            directory = (const char*)&argv[i][7];
+         }
+         else {
+            fprintf(stderr, "ERROR: Bad parameter \"%s\".\n", argv[i]);
+            exit(1);
+         }
+      }
+      else {
+         break;
+      }
+   }
+
    if( (setuid(0) == 0) && (setgid(0) == 0) ) {
-      execve(argv[1], &argv[1], NULL);
+      if(directory) {
+         if(chdir(directory) != 0) {
+            perror("Unable to change directory");
+            exit(1);
+         }
+      }
+      execve(argv[programArg], &argv[programArg], NULL);
       perror("Unable to start program");
    }
    else {
