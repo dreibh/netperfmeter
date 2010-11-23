437f689b6569a8bb080f6e482c09249aba667546
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2010-11-23 18:08:16 +0000

    Added PAMS2011 plot script for measurement results.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@813 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

---
+++ src/plot-PAMS2011.R
@@ -0,0 +1,53 @@
+# ###########################################################################
+# Name:        wp3-dsl-bufferSplittingI
+# Description: Buffer Splitting on DSL setup
+# Revision:    $Id$
+# ###########################################################################
+
+source("plot-version1.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+# simulationDirectory  <- "wp3-dsl-unordered-bandwidthI"
+plotColorMode        <- cmColor
+plotHideLegend       <- FALSE
+plotLegendSizeFactor <- 0.8
+plotOwnOutput        <- TRUE
+plotFontFamily       <- "Helvetica"
+plotFontPointsize    <- 22
+plotWidth            <- 10
+plotHeight           <- 10
+plotConfidence       <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   # ------ Format example --------------------------------------------------
+   # list(simulationDirectory, "output.pdf",
+   #      "Plot Title",
+   #      list(xAxisTicks) or NA, list(yAxisTicks) or NA, list(legendPos) or NA,
+   #      "x-Axis Variable", "y-Axis Variable",
+   #      "z-Axis Variable", "v-Axis Variable", "w-Axis Variable",
+   #      "a-Axis Variable", "b-Axis Variable", "p-Axis Variable")
+   # ------------------------------------------------------------------------
+
+   list("wp3-dsl-unordered-bandwidthI", "PAMS2011-measurement-wp3-dsl-unordered-bandwidthI",
+        "Received Bit Rate per Flow", seq(0,10,1), seq(0,11,1), list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "OptionBufferSplitting", "OptionNRSACK", "",
+        "passive.flow", "OptionRP", "CwndMaxBurst",
+        "
+            ((data1$OptionBufferSplitting == \"none\") | (data1$OptionBufferSplitting == \"bothSides\"))
+        ",
+        "vSortAscending<-FALSE")
+)
+
+
+# ------ Variable templates -------------------------------------------------
+plotVariables <- append(list(
+), netPerfMeterPlotVariables)
+
+# ###########################################################################
+
+createPlots("PAMS2011", plotConfigurations)
