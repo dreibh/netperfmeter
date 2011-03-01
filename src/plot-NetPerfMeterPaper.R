7bb18eee762ec02564f958b76583fb9ccfdba095
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2011-03-01 13:29:59 +0000

    Added plot script for new paper.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@1007 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

---
+++ src/plot-NetPerfMeterPaper.R
@@ -0,0 +1,86 @@
+# ###########################################################################
+# Revision:    $Id$
+# ###########################################################################
+
+source("plot-version1.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+# simulationDirectory  <- "wp3-dsl-unordered-bandwidthI"
+plotColorMode        <- cmColor
+plotHideLegend       <- FALSE
+plotLegendSizeFactor <- 0.8
+plotOwnOutput        <- FALSE
+plotFontFamily       <- "Helvetica"
+plotFontPointsize    <- 22
+plotWidth            <- 10
+plotHeight           <- 10
+plotConfidence       <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   # ------ Format example --------------------------------------------------
+   # list(simulationDirectory, "output.pdf",
+   #      "Plot Title",
+   #      list(xAxisTicks) or NA, list(yAxisTicks) or NA, list(legendPos) or NA,
+   #      "x-Axis Variable", "y-Axis Variable",
+   #      "z-Axis Variable", "v-Axis Variable", "w-Axis Variable",
+   #      "a-Axis Variable", "b-Axis Variable", "p-Axis Variable")
+   # ------------------------------------------------------------------------
+
+   list("wp3-dsl-ccI", "NetPerfMeterPaper-wp3-dsl-ccI-SCTP-Main-SCTP.pdf",
+        "CMT-SCTP Flow", seq(0,20,2), seq(0,16,2), list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "CMTCCVariant", "OptionBufferSplitting", "OptionNRSACK",
+        "passive.flow", "ReferenceFlowProtocol", "",
+        "(data1$ReferenceFlowProtocol == \"SCTP\") &
+         (data1$passive.flow == 1)",
+        "vSortAscending<-FALSE"),
+   list("wp3-dsl-ccI", "NetPerfMeterPaper-wp3-dsl-ccI-SCTP-Reference.pdf",
+        "SCTP Reference Flow", seq(0,20,2), seq(0,16,2), list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "CMTCCVariant", "OptionBufferSplitting", "OptionNRSACK",
+        "passive.flow", "ReferenceFlowProtocol", "",
+        "(data1$ReferenceFlowProtocol == \"SCTP\") &
+         (data1$passive.flow == 1001)",
+        "vSortAscending<-FALSE"),
+
+   list("wp3-dsl-ccI", "NetPerfMeterPaper-wp3-dsl-ccI-TCP-Main-TCP.pdf",
+        "CMT-SCTP Flow", seq(0,20,2), seq(0,16,2), list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "CMTCCVariant", "OptionBufferSplitting", "OptionNRSACK",
+        "passive.flow", "ReferenceFlowProtocol", "",
+        "(data1$ReferenceFlowProtocol == \"TCP\") &
+         (data1$passive.flow == 1)",
+        "vSortAscending<-FALSE"),
+   list("wp3-dsl-ccI", "NetPerfMeterPaper-wp3-dsl-ccI-TCP-Reference.pdf",
+        "TCP Reference Flow", seq(0,20,2), seq(0,16,2), list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "CMTCCVariant", "OptionBufferSplitting", "OptionNRSACK",
+        "passive.flow", "ReferenceFlowProtocol", "",
+        "(data1$ReferenceFlowProtocol == \"TCP\") &
+         (data1$passive.flow == 1001)",
+        "vSortAscending<-FALSE")
+   
+)
+
+
+# ------ Variable templates -------------------------------------------------
+plotVariables <- append(netPerfMeterPlotVariables, list(
+#    list("RateNorthernTrail-Mbit",
+#            "Bandwidth on High-Speed Path{:beta:}[Mbit/s]",
+#            "data1$RateNorthernTrail / 1000"),
+#    list("passive.flow-ReceivedBitRate-Mbit",
+#            "CMT-SCTP Payload Throughput [Mbit/s]",
+#            "8 * data1$passive.flow.ReceivedByteRate / 1000000",
+#            "blue4",
+#            list("passive.flow-ReceivedByteRate")),
+#    list("OptionBufferSplitting", "Buffer Splitting{:Pi:}"),
+#    list("OptionNRSACK",          "Use NR-SACK{:rho:}")
+))
+
+# ###########################################################################
+
+createPlots("NetPerfMeterPaper", plotConfigurations)
