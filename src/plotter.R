08a0d5b6d96c0e3ac3f77307f254f4e1eaf56044
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2011-11-23 06:34:11 +0000

    Minor updates.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@1138 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/plotter.R
+++ src/plotter.R
@@ -1,7 +1,7 @@
 # $Id$
 # ###########################################################################
 #             Thomas Dreibholz's R Simulation Scripts Collection
-#                  Copyright (C) 2005-2010 Thomas Dreibholz
+#                  Copyright (C) 2005-2012 Thomas Dreibholz
 #
 #               Author: Thomas Dreibholz, dreibh@iem.uni-due.de
 # ###########################################################################
