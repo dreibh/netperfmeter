2222d748876f2acae573c609a426e47eeb5bf11b
adhari <adhari@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2012-11-16 13:46:30 +0000

    testscripts for local tests MPTCP
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@1536 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

---
+++ src/plot-haikou-mptcp-throughputI.R
@@ -0,0 +1,52 @@
+# ###########################################################################
+# Name:        wp1-cc-disjoint-bandwidth-betaI
+# Description: CCs in DSL setup
+# Revision:    $Id: plot-wp1-cc-disjoint-bandwidth-betaI.R 1255 2011-12-16 12:08:15Z adhari $
+# ###########################################################################
+
+source("plot-version1.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+simulationDirectory  <- "wp1-cc-disjoint-bandwidth-betaI"
+plotColorMode        <- cmColor
+plotHideLegend       <- FALSE
+plotLegendSizeFactor <- 0.8
+plotOwnOutput        <- FALSE
+plotFontFamily       <- "Helvetica"
+plotFontPointsize    <- 22
+plotWidth            <- 10
+plotHeight           <- 10
+plotConfidence       <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   # ------ Format example --------------------------------------------------
+   # list(simulationDirectory, "output.pdf",
+   #      "Plot Title",
+   #      list(xAxisTicks) or NA, list(yAxisTicks) or NA, list(legendPos) or NA,
+   #      "x-Axis Variable", "y-Axis Variable",
+   #      "z-Axis Variable", "v-Axis Variable", "w-Axis Variable",
+   #      "a-Axis Variable", "b-Axis Variable", "p-Axis Variable")
+   # ------------------------------------------------------------------------
+
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-ReceivedBitRate.pdf"),
+        "", NA, NA, list(0,1),
+        "RateNorthernTrail-Mbit", "passive.flow-ReceivedBitRate-Mbit",
+        "CMTCCVariant", "passive.flow", "OptionNRSACK",
+        "", "ReferenceFlowProtocol", "",
+        "TRUE", 
+	 "zColorArray <- c(\"darkgreen\", \"orange\", \"red\", \"blue\", \"gray50\")")
+)
+
+
+# ------ Variable templates -------------------------------------------------
+plotVariables <- append(list(
+), netPerfMeterPlotVariables)
+
+# ###########################################################################
+
+createPlots(simulationDirectory, plotConfigurations,
+            customFilter="sed -f change_names_Flows.sed")
