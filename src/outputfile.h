771cb40196380c87e81e36c9229b5dd5a10c0b8d
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2014-11-17 07:41:37 +0000

    Packaging script updates.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/netperfmeter@1933 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/outputfile.h
+++ src/outputfile.h
@@ -1,7 +1,7 @@
 /* $Id$
  *
  * Plain/BZip2 File Output
- * Copyright (C) 2009-2014 by Thomas Dreibholz
+ * Copyright (C) 2009-2015 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
