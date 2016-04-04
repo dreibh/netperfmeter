2ce12e6bb64bacaf85dc65ca4b034ca30c54cf2d
Thomas Dreibholz <dreibh@simula.no>
2016-04-04 15:23:58 +0200

    Some build improvements for Debian hardening.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash -e
+
+if [ -e /usr/bin/dpkg-buildflags ] ; then
+   export CPPFLAGS=`dpkg-buildflags --get CPPFLAGS`
+   export CFLAGS=`dpkg-buildflags --get CFLAGS`
+   export CXXFLAGS=`dpkg-buildflags --get CXXFLAGS`
+   export LDFLAGS=`dpkg-buildflags --get LDFLAGS`
+fi
 
 ./bootstrap && ./configure --enable-test-programs --enable-plotting-programs && make
