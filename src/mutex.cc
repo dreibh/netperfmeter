c0425f87a0b90612552abe8817e7dfa4a4c2e681
Thomas Dreibholz <dreibh@simula.no>
2017-10-29 20:09:37 +0100

    Header updates.

--- src/mutex.cc
+++ src/mutex.cc
@@ -1,7 +1,7 @@
 /*
  * ==========================================================================
  *                  NetPerfMeter -- Network Performance Meter                 
- *                 Copyright (C) 2009-2017 by Thomas Dreibholz
+ *                 Copyright (C) 2009-2018 by Thomas Dreibholz
  * ==========================================================================
  *
  * This program is free software: you can redistribute it and/or modify
