fd20a3e1d318ba0826cce02e34d2c05969e68218
Thomas Dreibholz <dreibh@simula.no>
2017-10-29 19:39:40 +0100

    Updated autogen.sh with FreeBSD support.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,17 @@
-#!/bin/sh -e
+#!/bin/bash
 
 rm -f CMakeCache.txt
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NEAT=0 -DBUILD_TEST_PROGRAMS=1 -DBUILD_PLOT_PROGRAMS=1 $@ .
 
-cores=`getconf _NPROCESSORS_ONLN`
-make -j${cores}
+# ------ Obtain number of cores ---------------------------------------------
+# Try Linux
+cores=`getconf _NPROCESSORS_ONLN 2>/dev/null`
+if [ "$cores" == "" ] ; then
+   # Try FreeBSD
+   cores=`sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2 | tr -d ' '`
+fi
+if [ "$cores" == "" ] ; then
+   cores="1"
+fi
+
+gmake -j$cores || make -j$cores
