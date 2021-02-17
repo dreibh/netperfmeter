d51770ff200ec9280f87cefabf2783956ea932e1
Thomas Dreibholz <dreibh@simula.no>
2021-02-17 15:16:37 +0100

    Header updates.

--- src/rootshell.c
+++ src/rootshell.c
@@ -8,7 +8,7 @@
  *
  * ------------- An Efficient RSerPool Prototype Implementation -------------
  *
- * Copyright (C) 2002-2020 by Thomas Dreibholz
+ * Copyright (C) 2002-2021 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
