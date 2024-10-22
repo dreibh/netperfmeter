c8648357b29d91b87b987604013937ade0422845
Thomas Dreibholz <dreibh@simula.no>
2024-10-22 12:54:18 +0200

    Added loglevel functions for improved logging feature.

---
+++ src/loglevel.cc
@@ -0,0 +1,150 @@
+/*
+ * ==========================================================================
+ *         _   _      _   ____            __ __  __      _
+ *        | \ | | ___| |_|  _ \ ___ _ __ / _|  \/  | ___| |_ ___ _ __
+ *        |  \| |/ _ \ __| |_) / _ \ '__| |_| |\/| |/ _ \ __/ _ \ '__|
+ *        | |\  |  __/ |_|  __/  __/ |  |  _| |  | |  __/ ||  __/ |
+ *        |_| \_|\___|\__|_|   \___|_|  |_| |_|  |_|\___|\__\___|_|
+ *
+ *                  NetPerfMeter -- Network Performance Meter
+ *                 Copyright (C) 2009-2024 by Thomas Dreibholz
+ * ==========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:  dreibh@simula.no
+ * Homepage: https://www.nntb.no/~dreibh/netperfmeter/
+ */
+
+#include "loglevel.h"
+#include "tools.h"
+
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+
+#include <fstream>
+#include <iostream>
+
+
+std::ostream* gStdLog        = &std::cerr;
+unsigned int  gLogLevel      = LOGLEVEL_ERROR;
+bool          gColorMode     = true;
+bool          gCloseStdLog   = false;
+char          gHostName[256] = { 0x00 };
+Mutex         gLogMutex;
+
+
+// ###### Initialise log file ###############################################
+static bool initLogFile(const unsigned int logLevel,
+                        const char*        fileName,
+                        const bool         appendMode)
+{
+   finishLogging();
+   if(fileName != NULL) {
+      std::ofstream* newLogFile =
+         (appendMode == true) ? new std::ofstream(fileName, std::ios_base::app) :
+                                new std::ofstream(fileName);
+      if(newLogFile != nullptr) {
+         if(newLogFile->good()) {
+            gStdLog = newLogFile;
+            gCloseStdLog = true;
+            gLogLevel    = std::max(logLevel, MIN_LOGLEVEL);
+            return true;
+         }
+         delete newLogFile;
+      }
+   }
+   return false;
+}
+
+
+// ###### Initialise logging ################################################
+bool initLogging(const char* parameter)
+{
+   if(!(strncmp(parameter,"-logfile=",9))) {
+      return(initLogFile(gLogLevel, (char*)&parameter[9], false));
+   }
+   else if(!(strncmp(parameter,"-logappend=",11))) {
+      return(initLogFile(gLogLevel, (char*)&parameter[11], true));
+   }
+   else if(!(strcmp(parameter, "-logquiet"))) {
+      initLogFile(0, nullptr, false);
+      gLogLevel = 0;
+   }
+   else if(!(strncmp(parameter, "-loglevel=", 10))) {
+      gLogLevel = std::max((unsigned int)atoi((char*)&parameter[10]), MIN_LOGLEVEL);
+   }
+   else if(!(strncmp(parameter, "-logcolor=", 10))) {
+      if(!(strcmp((char*)&parameter[10], "off"))) {
+         gColorMode = false;
+      }
+      else {
+         gColorMode = true;
+      }
+   }
+   else {
+      std::cerr << format("ERROR: Invalid logging parameter %s", parameter) << "\n";
+      return(false);
+   }
+   return(true);
+}
+
+
+// ###### Begin logging #####################################################
+void beginLogging()
+{
+   gLogMutex.lock();
+
+   if(gStdLog == nullptr) {
+      gStdLog      = &std::cerr;
+      gCloseStdLog = false;
+   }
+   if((gCloseStdLog) && (gStdLog->tellp() > 0)) {
+      *gStdLog << "\n"
+               << "#############################################################################"
+               << "\n\n";
+   }
+
+   utsname hostInfo;
+   if(uname(&hostInfo) != 0) {
+      strcpy((char*)&gHostName, "?");
+   }
+   else {
+      snprintf((char*)&gHostName, sizeof(gHostName), "%s",
+               hostInfo.nodename);
+   }
+
+   LOG_INFO
+   *gStdLog << format("Logging started, log level is %d.", gLogLevel)
+            << "\n";
+   LOG_END
+
+   gLogMutex.unlock();
+}
+
+
+// ###### Finish logging ####################################################
+void finishLogging()
+{
+   if((gStdLog) && (gCloseStdLog)) {
+      LOG_INFO
+      *gStdLog << "Logging finished.\n";
+      LOG_END
+      delete gStdLog;
+      gStdLog      = &std::cerr;
+      gCloseStdLog = false;
+   }
+}
---
+++ src/loglevel.h
@@ -0,0 +1,103 @@
+/*
+ * ==========================================================================
+ *         _   _      _   ____            __ __  __      _
+ *        | \ | | ___| |_|  _ \ ___ _ __ / _|  \/  | ___| |_ ___ _ __
+ *        |  \| |/ _ \ __| |_) / _ \ '__| |_| |\/| |/ _ \ __/ _ \ '__|
+ *        | |\  |  __/ |_|  __/  __/ |  |  _| |  | |  __/ ||  __/ |
+ *        |_| \_|\___|\__|_|   \___|_|  |_| |_|  |_|\___|\__\___|_|
+ *
+ *                  NetPerfMeter -- Network Performance Meter
+ *                 Copyright (C) 2009-2024 by Thomas Dreibholz
+ * ==========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:  dreibh@simula.no
+ * Homepage: https://www.nntb.no/~dreibh/netperfmeter/
+ */
+
+#ifndef LOGLEVEL_H
+#define LOGLEVEL_H
+
+#include "mutex.h"
+#include "tools.h"
+
+
+#define LOGLEVEL_FATAL   5U
+#define LOGLEVEL_ERROR   4U
+#define LOGLEVEL_WARNING 3U
+#define LOGLEVEL_INFO    2U
+#define LOGLEVEL_DEBUG   1U
+#define LOGLEVEL_TRACE   0U
+#define MIN_LOGLEVEL     LOGLEVEL_TRACE
+
+
+extern std::ostream* gStdLog;
+extern unsigned int  gLogLevel;
+extern bool          gColorMode;
+extern bool          gCloseStdLog;
+extern char          gHostName[256];
+extern Mutex         gLogMutex;
+
+#define stdlog (*gStdLog)
+
+#define LOG_BEGIN(ansiColorSeq)   \
+   {                              \
+      loggingMutexLock();         \
+      if(gColorMode) {            \
+        *gStdLog << ansiColorSeq; \
+      }                           \
+      printTimeStamp(*gStdLog);
+
+#define LOG_END                   \
+      if(gColorMode) {            \
+        *gStdLog << "\e[0m";      \
+      }                           \
+      loggingMutexUnlock();       \
+   }
+
+#define LOG_END_FATAL                          \
+      *gStdLog << "FATAL ERROR - ABORTING!\n"; \
+      setLogColor(0);                          \
+      *gStdLog->flush();                       \
+      abort();                                 \
+   }
+
+
+#define LOG_FATAL   if((LOGLEVEL_FATAL   >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_FATAL))   LOG_BEGIN("\e[37;41;1m")
+#define LOG_ERROR   if((LOGLEVEL_ERROR   >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_ERROR))   LOG_BEGIN("\e[31;1m")
+#define LOG_WARNING if((LOGLEVEL_WARNING >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_WARNING)) LOG_BEGIN("\e[33m")
+#define LOG_INFO    if((LOGLEVEL_INFO    >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_INFO))    LOG_BEGIN("\e[34m")
+#define LOG_DEBUG   if((LOGLEVEL_DEBUG   >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_DEBUG))   LOG_BEGIN("\e[36m")
+#define LOG_TRACE   if((LOGLEVEL_TRACE   >= MIN_LOGLEVEL) && (gLogLevel <= LOGLEVEL_TRACE))   LOG_BEGIN("\e[37m")
+
+
+bool initLogging(const char* parameter);
+void beginLogging();
+void finishLogging();
+
+inline void loggingMutexLock() {
+   gLogMutex.lock();
+}
+
+inline void loggingMutexUnlock() {
+   gLogMutex.unlock();
+}
+
+inline const char* getHostName()
+{
+   return((const char*)gHostName);
+}
+
+#endif
