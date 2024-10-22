057877cbbdcb19682217a0920312dbe60ff14d4c
Thomas Dreibholz <dreibh@simula.no>
2024-10-22 12:56:52 +0200

    Updated CMakeLists.txt.

---
+++ src/t1.cc
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "loglevel.h"
+
+int main(int argc, char** argv)
+{
+   for(int i = 1;i < argc;i++) {
+      if(!(strncmp(argv[i], "-log" ,4))) {
+         if(initLogging(argv[i]) == false) {
+            return(0);
+         }
+      }
+   }
+
+   LOG_FATAL
+   stdlog << "Fatal\n";
+   LOG_END
+
+   LOG_ERROR
+   stdlog << "Error\n";
+   LOG_END
+
+   LOG_WARNING
+   stdlog << "Warning\n";
+   LOG_END
+
+   LOG_INFO
+   stdlog << "Info\n";
+   LOG_END
+
+   LOG_DEBUG
+   stdlog << "Debug\n";
+   LOG_END
+
+   LOG_TRACE
+   stdlog << "Trace\n";
+   LOG_END
+
+   return 0;
+}
