5646c987d4bc5e9f1318ed2219e94739100221d0
Thomas Dreibholz <dreibh@simula.no>
2025-12-04 12:36:31 +0100

    Header updates.

--- src/simpleredblacktree.c
+++ src/simpleredblacktree.c
@@ -8,7 +8,7 @@
  *
  * ------------- An Open Source RSerPool Simulation for OMNeT++ -------------
  *
- * Copyright (C) 2003-2025 by Thomas Dreibholz
+ * Copyright (C) 2003-2026 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
