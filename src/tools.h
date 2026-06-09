295f3963b00330d65bb639e748f713efdf65478a
Thomas Dreibholz <dreibh@simula.no>
2026-06-09 16:55:09 +0200

    Some more clean-up.

--- src/tools.h
+++ src/tools.h
@@ -44,22 +44,28 @@
 #include <ext_socket.h>
 
 // Endianess conversions: htobe*(), be*toh():
-#if defined(__linux__)
+#if defined(__linux__) || defined(__OpenBSD__)
 #include <endian.h>
 #elif defined(__FreeBSD__) || defined(__NetBSD__)
 #include <sys/endian.h>
-#elif defined(__OpenBSD__)
-#include <endian.h>
 #elif defined(__APPLE__)
 #include <libkern/OSByteOrder.h>
+// Big Endian conversions:
 #define htobe16(x) OSSwapHostToBigInt16(x)
 #define be16toh(x) OSSwapBigToHostInt16(x)
 #define htobe32(x) OSSwapHostToBigInt32(x)
 #define be32toh(x) OSSwapBigToHostInt32(x)
 #define htobe64(x) OSSwapHostToBigInt64(x)
 #define be64toh(x) OSSwapBigToHostInt64(x)
+// Little Endian conversions:
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
 #else
-#error Unsupported system: add endianess conversion function definitions!
+#error Unsupported platform; add missing case here!
 #endif
 
 #include "mutex.h"
